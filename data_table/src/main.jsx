cf0c69b76c6ceaa985df789fa00947117cffe805
surajn581 <surajn581@gmail.com>
2024-04-09 00:41:09 +0530

    using createRoot & root.render instead of ReactDOM.render

--- data_table/src/main.jsx
+++ data_table/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <App />,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
